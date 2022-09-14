672eb3fb8e62b57161e9b8002918f6833aa3b56a
admin <admin>
2022-09-14 15:47:17 +1000

    Saved recipe: SMALLPROJECT.compute_example_dataset

---
+++ recipes/compute_example_dataset.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+example_ds_df = pd.DataFrame(data = pd.date_range(start = '2022-01-25', end = '2022-05-01'))
+
+
+# Write recipe outputs
+example_dataset = dataiku.Dataset("example_dataset")
+example_dataset.write_with_schema(example_dataset_df)
